3852462039c01ffa54ce193c813f60ce015600ac
Vladislavvvv11111 <g9139107733@gmail.com>
2026-06-17 12:55:30 +0300

    added docs

---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['do_20list_0',['Менеджер задач (To-Do List)',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ docs/html/search/pages_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['менеджер_20задач_20to_20do_20list_0',['Менеджер задач (To-Do List)',['../md__r_e_a_d_m_e.html',1,'']]]
+];
